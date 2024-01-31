fee01b3358c7dac9a9982b927d3ce0e971786b5a
Alnur <alnur.tazhbenov@mail.ru>
2024-01-31 13:54:10 +0300

    Add database and refactor orm.py class

---
+++ src/core/database/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.core.database.db_base import Base
+
+
+class PlayersOrm(Base):
+    __tablename__ = "players"
+
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    user_name: Mapped[str]
+    is_play: Mapped[bool]
+    extra_pl: Mapped[int] = mapped_column(default=0)
--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@ import sys
 
 from aiogram.methods import DeleteWebhook
 
-from src.bot_base import bot, create_pool, dp
+from src.bot_base import bot, dp
+from src.core.database.orm import AsyncOrm
 from src.core.handlers import basic, info
-from src.core.middlewares.dbmiddlewares import DbSession
 
 
 async def main() -> None:
@@ -15,8 +15,9 @@ async def main() -> None:
         format="%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
         stream=sys.stdout,
     )
-    pool_connect = await create_pool()
-    dp.update.middleware.register(DbSession(pool_connect))
+    await AsyncOrm.create_tables()
+    await AsyncOrm.insert_players(123456789, "Alnur", True, 5)
+
     basic.register_basic_handlers()
     info.register_info_handlers()
 
