2968c178a376aa89b5f797e54068d1e34e8193a4
Alnur <alnur.tazhbenov@mail.ru>
2024-02-06 15:45:58 +0300

    feat(main and basic): add output of players from the database to the message body

--- src/core/handlers/basic.py
+++ src/core/handlers/basic.py
@@ -3,20 +3,52 @@ from aiogram.types import Message
 from aiogram.utils.formatting import as_list, as_numbered_list
 
 from src.bot_base import dp
+from src.core.database.orm import AsyncOrm
 from src.core.keyboards.inline import get_inline_keyboard
 
 
-@dp.message(Command("poll"))
-async def get_pool(message: Message):
-    text = as_list(
+async def send_text():
+    players = await AsyncOrm.get_players_list()
+    not_players = await AsyncOrm.get_not_players_list()
+    players_name_list = [
+        pl.user_name if pl.is_play else f"{pl.user_name} +1" for pl in players
+    ]
+    not_players_name_list = [pl.user_name for pl in not_players]
+
+    return as_list(
         "Игра в четверг\n",
         "Участники:",
-        as_numbered_list(*["Alnur", "Talga", "Lena"], fmt="{}.    "),
+        as_numbered_list(*players_name_list, fmt="{}.    "),
+        "",
+        as_numbered_list(*not_players_name_list, fmt="{}.    "),
     )
+
+
+async def test_func():
+    await AsyncOrm.add_player(123456789, "Alnur", False, 5)
+    await AsyncOrm.add_player(567891234, "Talga", False, 2)
+    await AsyncOrm.add_player(912345678, "Lena", True, 0)
+    await AsyncOrm.add_player(912567834, "Kamil", False, 0)
+    await AsyncOrm.update_player_status(567891234, is_play=True)
+    await AsyncOrm.update_player_status(123456789, extra_player=1)
+    await AsyncOrm.update_player_status(567891234, extra_player=-1)
+
+
+@dp.message(Command("poll"))
+async def get_poll(message: Message):
+    await AsyncOrm.create_tables()
+    await test_func()
+    text_for_poll = await send_text()
     await message.answer(
-        **text.as_kwargs(),
+        **text_for_poll.as_kwargs(),
         reply_markup=get_inline_keyboard(),
     )
+
+
+# @dp.callback_query()
+# async def play_game(call: CallbackQuery):
+#     if call.data == "is_play_true":
+#         await call.message.answer("Ar")
 
 
 def register_basic_handlers():
--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import sys
 from aiogram.methods import DeleteWebhook
 
 from src.bot_base import bot, dp
-from src.core.database.orm import AsyncOrm
 from src.core.handlers import basic, info
 
 
@@ -15,15 +14,6 @@ async def main() -> None:
         format="%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s",
         stream=sys.stdout,
     )
-    class_async = AsyncOrm()
-    await class_async.create_tables()
-    await class_async.add_player(123456789, "Alnur", True, 5)
-    await class_async.add_player(567891234, "Talga", True, 2)
-    await class_async.update_player_status(567891234, is_play=False)
-    await class_async.update_player_status(123456789, extra_player=1)
-    await class_async.update_player_status(567891234, extra_player=-1)
-    await class_async.update_players_list()
-    print([x.user_name for x in class_async.players_list])
 
     basic.register_basic_handlers()
     info.register_info_handlers()
