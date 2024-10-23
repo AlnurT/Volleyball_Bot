6de48c17a1df2cfb7aaef89b275b8c5948a6ff3b
Alnur <alnur.tazhbenov@mail.ru>
2024-10-23 14:11:18 +0300

    feat: add new rules and phone number for web

--- web_app/app.js
+++ web_app/app.js
@@ -2,7 +2,10 @@ let tg = window.Telegram.WebApp;
 let info = document.getElementById("info_btn");
 let rules = document.getElementById("rules_btn");
 let address = document.getElementById("address");
-let tel = document.getElementById("tel");
+let tel_admin = document.getElementById("tel_admin");
+let tel_capt = document.getElementById("tel_capt");
+let players = document.getElementById("players");
+let price = document.getElementById("price");
 
 
 info.addEventListener("click", () => {
@@ -23,12 +26,17 @@ address.addEventListener("click", () => {
     tg.openLink("https://yandex.ru/maps/org/klub_voinskogo_iskusstva_teskao_bely_volk/1694603185/?ll=30.339000%2C59.967488&z=18");
 });
 
-tel.addEventListener("click", () => {
+tel_admin.addEventListener("click", () => {
+    navigator.clipboard.writeText('+79119673502');
+    tg.showAlert("Телефон скопирован");
+});
+
+tel_capt.addEventListener("click", () => {
     navigator.clipboard.writeText('+79006200637');
     tg.showAlert("Телефон скопирован");
 });
 
 players.oninput = function () {
     let pl = players.value
-    price.innerHTML = (pl < 8 || pl > 14) ? "ошибка" : Math.round(3500 / pl);
+    price.innerHTML = (pl < 8 || pl > 18) ? "ошибка" : Math.round(3500 / pl);
 }
